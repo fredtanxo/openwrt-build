fb14be46425a0e633b106a11e5256f5535bcb384
Fred Tan <fredtan4go@gmail.com>
2023-02-06 22:41:38 +0800

    Rearrange workflow steps

--- customize.sh
+++ customize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Modify default LAN IP
-sed -i 's/192.168.1.1/192.168.88.1/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.1/g' ./openwrt/package/base-files/files/bin/config_generate
 
 # Enable fullcone NAT
-sed -i '/option forward\t\tREJECT.*/a\\toption fullcone\t\t1' ./package/network/config/firewall/files/firewall.config
+sed -i '/option forward\t\tREJECT.*/a\\toption fullcone\t\t1' ./openwrt/package/network/config/firewall/files/firewall.config
 
 # Change default timezone to Asia/Shanghai
-sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
-sed -i "/.*ttylogin='0'.*/i\\\t\tset system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+sed -i "s/timezone='.*'/timezone='CST-8'/g" ./openwrt/package/base-files/files/bin/config_generate
+sed -i "/.*ttylogin='0'.*/i\\\t\tset system.@system[-1].zonename='Asia/Shanghai'" ./openwrt/package/base-files/files/bin/config_generate
